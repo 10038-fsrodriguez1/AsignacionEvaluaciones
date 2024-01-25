280b4ebdf648681d9d8687aec80588edafe3df91
Freddy Sebastián Rodríguez Alvear <107556691+10038-fsrodriguez1@users.noreply.github.com>
2024-01-24 22:39:16 -0500

    V1

---
+++ database/adopters.js
@@ -0,0 +1,17 @@
+
+module.exports = [
+    { "id": 1, 
+    "name": "Rodriguez",
+     "address": "123 Main St" 
+    },
+
+    { "id": 2, 
+    "name": "Mora",
+     "address": "456 Oak St" 
+    },
+    
+    { "id": 3, 
+    "name": "Garcia",
+     "address": "789 Pine St" 
+    }
+];
---
+++ database/dogs.js
@@ -0,0 +1,14 @@
+
+module.exports = [
+    { "id": 1,
+     "name": "Metodos Numericos" 
+    },
+
+    { "id": 2, 
+    "name": "Fundameos de programacion"
+    },
+
+    { "id": 3,
+     "name": "Seguridad Informatica" 
+    }
+];
